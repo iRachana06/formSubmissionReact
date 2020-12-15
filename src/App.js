c16bf0596be7c908f36eca443d64bb2d694500ce
iRachana <47643426+iRachana@users.noreply.github.com>
2020-12-15 11:55:01 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+          <p>Header</p>
+          <p>Content</p>
+     </div>
+    );
+  }
 }
+
 
 export default App;
